3e55a8a6a4278f8aed814010719adda0d497f7e5
JelsonRodrigues <jelsonrodrigues25@gmail.com>
2022-04-07 21:34:13 -0300

    Algoritmo de Dijkstra completo

--- ALGORITMOS E ESTRUTURAS DE DADOS II/Semana 03/dijkstra.c
+++ ALGORITMOS E ESTRUTURAS DE DADOS II/Semana 03/dijkstra.c
@@ -27,6 +27,7 @@ void imprimeNodo(Nodo *no);
 Nodo lerNodo();
 
 /* Grafo */
+// A estrutura do grafo é composta de um vetor de nós e uma matriz com os pesos
 typedef struct {
     Nodo *nos;
     uint numero_nos;
@@ -38,10 +39,11 @@ bool nodoPertenceAoGrafo(Grafo *grafo, Nodo no);
 void alterarConexoesNo(Grafo *grafo);
 void imprimirGrafo(Grafo *g1);
 void liberarGrafo(Grafo *g1);
-int maiorStringTamanho(Nodo *nos, int numero_nos);
+uint maiorStringTamanho(Nodo *nos, int numero_nos);
+int procuraNodo(Grafo *grafo, Nodo no);
 
 
-// Dijktra
+/* Dijktra */
 void mostrarMenorCaminho(Grafo *grafo);
 typedef struct {
     bool vertice_aberto;
@@ -49,12 +51,15 @@ typedef struct {
     float menor_caminho;
 } AuxDijkstra;
 AuxDijkstra *inicializaVetoresDijkstra(Grafo *grafo);
-int *Dijkstra(Grafo *grafo, AuxDijkstra *aux_dijkstra);
+AuxDijkstra *Dijkstra(Grafo *grafo, int indice_vertice_inicial);
+bool possuiVerticesAbertos(AuxDijkstra *aux_dijkstra, int total_nos);
+int indiceVerticeAbertoComMenorEstimativa(AuxDijkstra *aux_dijkstra, int numero_nos);
 
 typedef struct {
     int *verticesAdjacentes;
     int tamanho;
 } VerticesAdjacentes;
+VerticesAdjacentes verticesAdjacentes(Grafo *grafo, int posicao_no);
 
 
 
@@ -145,7 +150,7 @@ OpcoesMenu menu(){
 }
 
 void limparEntrada(FILE *entrada){
-    while (getc(entrada) != EOF);
+    while (getc(entrada) != '\n');
 }
 
 Nodo criaNodo(char *nome){
@@ -216,27 +221,28 @@ void imprimirGrafo(Grafo *g1) {
             return;
         }
 
-        int tamanho_maior_nome = maiorStringTamanho(g1->nos, g1->numero_nos);
+        uint tamanho_maior_nome = maiorStringTamanho(g1->nos, g1->numero_nos);
+        if (tamanho_maior_nome < 4) tamanho_maior_nome += 4;
 
         // Mostra o nome de cada coluna
-        printf("%16s", " | ");
+        printf("%*s", tamanho_maior_nome + 3,  " | ");
         for (uint c = 0; c < g1->numero_nos; c++){
-            printf("%-13s | ", g1->nos[c].nome_cidade);
+            printf("%-*s | ", tamanho_maior_nome,  g1->nos[c].nome_cidade);
         }
 
         // Imprime cada linha e o conteúdo
         for (uint c = 0; c < g1->numero_nos; c++){
-            printf("\n%+13s | ", g1->nos[c].nome_cidade);
+            printf("\n%*s | ", tamanho_maior_nome, g1->nos[c].nome_cidade);
             for (uint i = 0; i < g1->numero_nos; i++) {
-                printf("%-13.3f | ", g1->pesos[c][i]);
+                printf("%-*.3f | ", tamanho_maior_nome,  g1->pesos[c][i]);
             }
         }
 
     }
 }
 
-int maiorStringTamanho(Nodo *nos, int numero_nos) {
-    int maior = 0;
+uint maiorStringTamanho(Nodo *nos, int numero_nos) {
+    uint maior = 0;
 
     for (int c = 0; c < numero_nos; c++) {
         if (c == 0) {
@@ -246,12 +252,11 @@ int maiorStringTamanho(Nodo *nos, int numero_nos) {
         if (strlen(nos[c].nome_cidade) > maior) {
             maior = strlen(nos[c].nome_cidade);
         }
-
     }
 
     return maior;
-
 }
+
 void alterarConexoesNo(Grafo *grafo) {
     if (!grafo) return;
     for (uint c = 0; c < grafo->numero_nos; c++) {
@@ -313,33 +318,51 @@ void mostrarMenorCaminho(Grafo *grafo) {
     }
 
     // Lê o vértice inicial para procura
-    Nodo vertice_partida;
+    
+    int indice_vertice_partida = -1;
     do {
         printf("\nVértice de início: ");
-        vertice_partida = lerNodo();
-    } while (procuraNodo(grafo, vertice_partida) == -1);
+        Nodo vertice_partida = lerNodo();
+        indice_vertice_partida = procuraNodo(grafo, vertice_partida);
+        free(vertice_partida.nome_cidade);
+    } while ( indice_vertice_partida == -1);
 
     // Lê o vértice de destino do menor caminho
-    Nodo vertice_chegada;
-        do {
+    int indice_vertice_chegada = -1;
+    do {
         printf("\nVérice de destino: ");
-        vertice_chegada = lerNodo();
-    } while (procuraNodo(grafo, vertice_chegada) == -1);
+        Nodo vertice_chegada = lerNodo();
+        indice_vertice_chegada = procuraNodo(grafo, vertice_chegada);
+        free(vertice_chegada.nome_cidade);
+    } while (indice_vertice_chegada == -1);
 
-    AuxDijkstra *aux_dijkstra = inicializaVetoresDijkstra(grafo);
 
-    aux_dijkstra[procuraNodo(grafo, vertice_partida)].menor_caminho = 0.0;
+    AuxDijkstra *resultado = Dijkstra(grafo, indice_vertice_partida);
+    if (!resultado) {
+        printf("\nERRO AO PROCURAR MENOR CAMINHO\n");
+        return;
+    }
 
-    Dijkstra(grafo, aux_dijkstra);
+    if (resultado[indice_vertice_chegada].menor_caminho == INFINITY) {
+        printf("\nNão existe um caminho entre os vértices selecionados\n");
+    }
+    else {
+        printf("\nO menor caminho é : %.2f\n", resultado[indice_vertice_chegada].menor_caminho);
 
-    free(aux_dijkstra);
-
+        int contador_vertice = indice_vertice_chegada;
+        while (contador_vertice != -1) {    // contador_vertice != indice_vertice_partida
+            printf("%s\n", grafo->nos[contador_vertice].nome_cidade);
+            contador_vertice = resultado[contador_vertice].indice_vertice_anterior;
+        }
+    }
+    free(resultado);
+    
 }
 
 AuxDijkstra *inicializaVetoresDijkstra(Grafo *grafo) {
     if (!grafo) return NULL;
     AuxDijkstra *temp = (AuxDijkstra *) malloc(sizeof(AuxDijkstra) * grafo->numero_nos);
-    for (int c = 0; c < grafo->numero_nos; c++) {
+    for (uint c = 0; c < grafo->numero_nos; c++) {
         temp[c].indice_vertice_anterior = -1;
         temp[c].menor_caminho = (float)INFINITY;   // Inicializa com infinito
         temp[c].vertice_aberto = true;
@@ -347,55 +370,60 @@ AuxDijkstra *inicializaVetoresDijkstra(Grafo *grafo) {
     return temp;
 }
 
-int *Dijkstra(Grafo *grafo, int indice_vertice_inicial){
-    if (!grafo || indice_vertice_inicial < 0) return (int *) NULL;
-
-    AuxDijkstra *aux_dijkstra = (AuxDijkstra *) malloc(sizeof(AuxDijkstra) * grafo->numero_nos);
-    if (!aux_dijkstra) {
-        return (int *) NULL;
-    }
-
+AuxDijkstra *Dijkstra(Grafo *grafo, int indice_vertice_inicial){
+    if (!grafo || indice_vertice_inicial < 0) return NULL;
+    
     // Inicialização 
-    for (int c = 0; c < grafo->numero_nos; c++) {
-        if (grafo->pesos[indice_vertice_inicial][c] != 0.0) { // adjacente
-            aux_dijkstra[c].indice_vertice_anterior = indice_vertice_inicial;
-            aux_dijkstra[c].menor_caminho = grafo->pesos[indice_vertice_inicial][c];
-        }
-        else {
-            aux_dijkstra[c].indice_vertice_anterior = -1;
-            aux_dijkstra[c].menor_caminho = (float)INFINITY;   // Inicializa com infinito
-        }
-        aux_dijkstra[c].vertice_aberto = true;
-    }
+    AuxDijkstra *aux_dijkstra = inicializaVetoresDijkstra(grafo);
 
-    aux_dijkstra[indice_vertice_inicial].vertice_aberto = false;
+    // Inicializa o vértice de partida com 0.0
+    aux_dijkstra[indice_vertice_inicial].menor_caminho = 0.0;
 
+    while (possuiVerticesAbertos(aux_dijkstra, grafo->numero_nos)) {
+        // Escolhe dentre os vértices abertos o que tem a menor estimativa
+        int indice_vertice_atual = indiceVerticeAbertoComMenorEstimativa(aux_dijkstra, grafo->numero_nos);
+        VerticesAdjacentes v_adj = verticesAdjacentes(grafo, indice_vertice_atual);
 
+        // Marca o vértice como fechado
+        aux_dijkstra[indice_vertice_atual].vertice_aberto = false;
 
-    int indice_vertice_anterior = indice_vertice_inicial;
-    while (possuiVerticesAbertos(aux_dijkstra, grafo->numero_nos)){
-        VerticesAdjacentes v_adj = verticesAdjacentes(grafo, indice_vertice_anterior);
-
-        int indice_menor_caminho_adjacente = -1;
-        // Determina o menor caminho 
-        for (int c = 0; c < v_adj.tamanho; c++){
-            if (c == 0 || aux_dijkstra[c].menor_caminho < aux_dijkstra[indice_menor_caminho_adjacente].menor_caminho){
-                indice_menor_caminho_adjacente = c;
+        // Atualiza os caminhos dos vértices adjacentes
+        for (int c=0; c < v_adj.tamanho; c++) {
+            int indice_vertice_adjacente = v_adj.verticesAdjacentes[c];
+            // Só visita vértices marcados como aberto
+            if (aux_dijkstra[indice_vertice_adjacente].vertice_aberto) {
+                if (aux_dijkstra[indice_vertice_atual].menor_caminho + grafo->pesos[indice_vertice_atual][indice_vertice_adjacente] < aux_dijkstra[indice_vertice_adjacente].menor_caminho){
+                    aux_dijkstra[indice_vertice_adjacente].menor_caminho = aux_dijkstra[indice_vertice_atual].menor_caminho + grafo->pesos[indice_vertice_atual][indice_vertice_adjacente];
+                    aux_dijkstra[indice_vertice_adjacente].indice_vertice_anterior = indice_vertice_atual;
+                }
             }
         }
 
-        // Atualiza os caminhos dos vértices adjacentes
-        
-
+        free(v_adj.verticesAdjacentes);
     }
 
+    return aux_dijkstra;
+}
 
+int indiceVerticeAbertoComMenorEstimativa(AuxDijkstra *aux_dijkstra, int numero_nos) {
+    int indice_vertice_menor_estimativa = -1;
+    float menor_estimativa = INFINITY;
+    for (int c = 0; c < numero_nos; c++){
+        if (aux_dijkstra[c].vertice_aberto == true){
+            if (indice_vertice_menor_estimativa == -1 || aux_dijkstra[c].menor_caminho < menor_estimativa){
+                indice_vertice_menor_estimativa = c;
+                menor_estimativa = aux_dijkstra[c].menor_caminho;
+            }
+        }
+    }
+    return indice_vertice_menor_estimativa;
 }
 
 bool possuiVerticesAbertos(AuxDijkstra *aux_dijkstra, int total_nos) {
     for (int c = 0; c < total_nos; c++){
         if (aux_dijkstra[c].vertice_aberto) return true;
     }
+
     return false;
 }
 
@@ -410,17 +438,16 @@ VerticesAdjacentes verticesAdjacentes(Grafo *grafo, int posicao_no){
         return temp_v;
     }
 
-    for (int c = 0; c < grafo->numero_nos; c++){
+    // Salva o índice dos vértices que são adjacentes (peso da conexão diferente de zero)
+    for (uint c = 0; c < grafo->numero_nos; c++){
         if (grafo->pesos[posicao_no][c] != 0.0){
             temp_v.verticesAdjacentes[temp_v.tamanho++] = c;
         }
     }
 
-
     // Diminui o espaço que não está sendo utilizado
-    realloc(temp_v.verticesAdjacentes, sizeof(int) * temp_v.tamanho);
+    temp_v.verticesAdjacentes = realloc(temp_v.verticesAdjacentes, sizeof(int) * temp_v.tamanho);
     if (!temp_v.verticesAdjacentes) {
-        printf("ERRO\n");
         temp_v.tamanho = 0;
     }
 
@@ -430,7 +457,7 @@ VerticesAdjacentes verticesAdjacentes(Grafo *grafo, int posicao_no){
 // Retorna o índice do nó, ou -1 caso não encontrar
 int procuraNodo(Grafo *grafo, Nodo no){
     if (grafo) {
-        for (int c = 0; c < grafo->numero_nos; c++){
+        for (uint c = 0; c < grafo->numero_nos; c++){
             if (strcmp(no.nome_cidade, grafo->nos[c].nome_cidade) == 0) return c;
         }
     }
