ac796af825bfba7172b85ef3c2f4d55c9180b6aa
Jelson Stoelben Rodrigues <jelsonrodrigues25@gmail.com>
2022-08-30 13:12:41 -0300

    Versão final do script para automação dos testes

--- ARQUITETURA E ORGANIZAÇÃO DE COMPUTADORES II/Trabalho 01/extracao_informacoes_arquivos.py
+++ ARQUITETURA E ORGANIZAÇÃO DE COMPUTADORES II/Trabalho 01/extracao_informacoes_arquivos.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import glob
-
+import os
 
 pastas = [
     "/home/jelson/Downloads/Resultados/EX01/Instrucoes/",
@@ -14,6 +14,9 @@ pastas = [
 ]
 
 for pasta in pastas:
+    if os.path.isdir(pasta) == False:
+        continue
+
     arquivos = glob.glob(pathname=pasta + "*out", recursive=True)
 
     informacoes = pd.DataFrame(columns=['Total Instrucoes Executadas', 'Leitura/Escritas', 'Acessos', 'Hits', 'Misses', 'Benchmark'])
@@ -29,19 +32,20 @@ for pasta in pastas:
                 continue
 
             estatisticas = saida_bruta.readlines()
-            total_instrucoes = int(estatisticas[-29][25:estatisticas[-29].find('#')])
-            leitura_escritas = int(estatisticas[-28][25:estatisticas[-28].find('#')])
-            acessos = int(estatisticas[-25][25:estatisticas[-25].find('#')])
-            misses = int(estatisticas[-24][25:estatisticas[-24].find('#')])
-            hits = int(estatisticas[-23][25:estatisticas[-23].find('#')])
+
+            total_instrucoes = int(estatisticas[0][estatisticas[0].find(' '):estatisticas[0].find('#')])
+            leitura_escritas = int(estatisticas[1][estatisticas[1].find(' '):estatisticas[1].find('#')])
+            acessos = int(estatisticas[4][estatisticas[4].find(' '):estatisticas[4].find('#')])
+            hits = int(estatisticas[5][estatisticas[5].find(' '):estatisticas[5].find('#')])
+            misses = int(estatisticas[6][estatisticas[6].find(' '):estatisticas[6].find('#')])
 
             informacoes = pd.concat([informacoes, pd.Series(
                 {
                 'Total Instrucoes Executadas': total_instrucoes,
                 'Leitura/Escritas': leitura_escritas,
                 'Acessos' : acessos,
-                'Hits' : misses,
-                'Misses' : hits,
+                'Hits' : hits,
+                'Misses' : misses,
                 'Benchmark' : arquivo[arquivo.rfind('/')+1:]
                 }
             ).to_frame().T], ignore_index=True)
